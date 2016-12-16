9fd6c5ad0fde3adce0e6e5eba3e1c0f6554f83b3
Gaurav Varshney <gaurav230188@gmail.com>
2016-12-16 16:28:57 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 function login()
-			{
+{
 $("#email").keypress(function(){
 $("#email_error_msg").fadeOut();
 });
 $("#password").keypress(function(){
-$("#password_error_msg").fadeOut();			
-});			
+$("#password_error_msg").fadeOut();         
+});         
 
-			var email =$("#email").val();
-	
-			var password=$("#password").val();
-			
-				if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))  
+            var email =$("#email").val();
+    
+            var password=$("#password").val();
+            
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))  
                 {  
                   verify_err=true; 
                 } 
@@ -21,56 +21,108 @@ $("#password_error_msg").fadeOut();
                verify_err=false;
                 if(email=='')
                 {
-                	$('#email_error_msg').html('please enter your email');
+                    $('#email_error_msg').html('please enter your email');
                 }
                 }
                
-				if(password=='')
-				{
-					$('#password_error_msg').html('please enter your Password');
-				}
-				
-				else
-				{
-				$.post('http://yoneak.com/web_services/login.php?email='+email+'&password='+password,function(res){
+                if(password=='')
+                {
+                    $('#password_error_msg').html('please enter your Password');
+                }
+                
+                else
+                {
+
+                $.post('http://yoneak.com/web_services/login.php?email='+email+'&password='+password,function(res){
         if(res.message == "Login Success")
         {  
             localStorage.setItem("userID",res.user_id);
+            var redirectPage=0;
             if((localStorage.url===null) || !(localStorage.url))
             {
-				 window.location='account.html';
-			}
-			else{
-				
-	
-				var userId=localStorage.userID;
-				var affiliateId=localStorage.affiliateId;
-				var url=localStirage.url;
-				var catName=localStirage.catName;
-				var catId=localStirage.catId;
-		$.post('http://yoneak.com/web_services/track_offer.php?userId='+userId+'&affiliateId='+affiliateId+'&referralUrl='+url+'&catName'+catName+'catId'+catId,function(res){
-			
-			if(res.message=="success")
-			{
-				window.location=url;
-			}
-			else{
-				alert("Something Went wrong");
-				window.location="account.html";
-			}
-		localStorage.removeItem("url");
-		localStorage.removeItem("catName");
-		localStorage.removeItem("affiliateId");
-		localStorage.removeItem("catId");
-			
-		},"json");
-			}
-           						
+                 if((localStorage.proName===null) || !(localStorage.proName))
+                {
+                     if((localStorage.cart===null) || !(localStorage.cart))
+                     {
+                           redirectPage=0;
+                     }
+                     else{
+                        redirectPage=3;
+                     }
+                }
+                else
+                {
+                    redirectPage=2;
+                }
+                 
+            }
+            else
+            {
+                redirectPage=1;
+            }
+            if(redirectPage==1){
+                
+    
+                var userId=localStorage.userID;
+                var affiliateId=localStorage.affiliateId;
+                var url=localStorage.url;
+                var catName=localStorage.catName;
+                var catId=localStorage.catId;
+                
+        $.post('http://yoneak.com/web_services/track_offer.php?userId='+userId+'&affiliateId='+affiliateId+'&referralUrl='+url+'&catName'+catName+'catId'+catId,function(res){
+            
+            if(res.message=="success")
+            {
+                window.location=url;
+            }
+            else{
+                alert("Something Went wrong");
+                window.location="account.html";
+            }
+        localStorage.removeItem("url");
+        localStorage.removeItem("catName");
+        localStorage.removeItem("affiliateId");
+        localStorage.removeItem("catId");
+            
+        },"json");
+            }
+            
+            else if(redirectPage==2)
+            {
+                var userId=localStorage.userID;
+                var proName=localStorage.proName;
+                var url=localStorage.url;
+                var proId=localStorage.proId;
+       $.post('http://yoneak.com/web_services/track_offer2.php?userId='+userId+'&affiliateId='+proId+'&referral_url='+url+'&product_name'+proName,function(res){
+            
+            if(res.message=="success")
+            {
+                window.location=url;
+            }
+            else{
+                alert("Something Went wrong");
+                window.location="account.html";
+            }
+        localStorage.removeItem("url");
+        localStorage.removeItem("proName");
+        localStorage.removeItem("proId");
+            
+        },"json");
+            }
+            else if(redirectPage==3)
+            {
+                 window.location='cartDetail.html';
+            }
+            else if(redirectPage==0)
+            {
+                 window.location='account.html';
+            }
+                                
            
         }
         if(res.message == "invalid Email Or Password")
         {
-        	$('#email_error_msg').html('invalid Email Or Password');
+            $('#email_error_msg').html('invalid Email Or Password');
         }
         
     }, "json").fail(function(){
